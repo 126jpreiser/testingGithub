fab626dab726fea593de2d903fc691d284cf33ac
nickrigatoni <113187682+nickrigatoni@users.noreply.github.com>
2024-02-20 12:21:21 -0500

    i am _ years old

--- First Github/src/Jayson_Preiser.java
+++ First Github/src/Jayson_Preiser.java
@@ -2,6 +2,11 @@ public class Jayson_Preiser {
     public static void Main(String[] args)
     {
         System.out.println("Hi Jayson!");
+
         System.out.println("Hello Jayson and Niyati!!");
+
+        for (int i=0; i<15; i++){
+            System.out.println("I am " + i + " years old");
+        }
     }
 }
